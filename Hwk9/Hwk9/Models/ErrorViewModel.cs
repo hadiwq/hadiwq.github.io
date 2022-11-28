2d235487bad9cfde125bf7dfbf984f961d5e6728
hadiwq <98136543+hadiwq@users.noreply.github.com>
2022-11-28 00:39:02 -0600

    Delete Hwk9 directory

--- Hwk9/Hwk9/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hwk9.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
